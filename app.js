34ecee7c54fd7a72071dd8638a9f7468db04ddb4
mengdev40150 <6631501154@lamduan.mfu.ac.th>
2025-08-24 17:04:22 +0700

    update api login

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const con = require('./db');
 // login
 app.post('/login', (req, res) => {
     const {username, password} = req.body;
-    const sql = "SELECT id, password FROM users WHERE username = ?";
+    const sql = "SELECT * FROM users WHERE username = ?";
     con.query(sql, [username], function(err, results) {
         if(err) {
             return res.status(500).send("Database server error");
@@ -34,15 +34,34 @@ app.post('/login', (req, res) => {
                 return res.status(500).send("Hashing error");
             }
             if(same) {
-                return res.send("Login OK");
+                return res.json({ 
+                    message: "Login OK", 
+                    id: results[0].id,
+                    username: results[0].username
+                });
             }
             return res.status(401).send("Wrong username or password");
         });
     })
 });
 
+
 // expense routes
 //======= Route fisrt =======
+// show all expense by user id
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    const sql = "SELECT* FROM expense WHERE user_id = ?";
+    con.query(sql, [userId], function(err, results) {
+        if(err) {
+            return res.status(500).send("Database server error");
+        }
+        if(results.length === 0) {
+            return res.status(404).send("no data found");
+        }
+        res.json(results);
+    });
+});
 
 //======= Route second =======
 
